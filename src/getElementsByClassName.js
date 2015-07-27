2013cfb64e9e2baef8a7b223c350ca26ee16eb64
MKS-students <mks.students@hackreactor.com>
2015-07-27 11:45:47 -0500

    completed recusion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,38 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
+
+//make array
+
+
+var results = [];
+
+
+var search = function(node){
+//check for class == className
+
+var parts = node.className.split(" ");
+
+if (parts.indexOf(className) >= 0){
+
+//if className save
+  results.push(node);
+}
+
+
+//if not check if Check if Children
+
+//if Children then iterate over children
+  for (var i = 0; i<node.children.length; i++){
+
+    search(node.children[i]);
+  }
+}
+
+
+search(document.body);
+
+return results;
+
   // your code here
 };
